ae8de82e4b0d03e5e3c10f53f6311bd51f9a6f98
Vincentyun <vincentzyc@qq.com>
2019-01-16 17:46:40 +0800

    GitHub Pages

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,15 +1,21 @@
 export default {
+	env() {
+		if (process.env.NODE_ENV === "development") return "development";
+		return "production";
+	},
 	previewUrl() {
-		if (window.location.port) {
+		if (this.env() === "development") {
 			let arr = window.location.origin.split(":");
 			return `${arr[0]}:${arr[1]}:3000`
 		}
 		return window.location.origin + "/form-design-h5"
 	},
-	env() {
-		if (process.env.NODE_ENV === "development") return "development";
-		if (window.location.href.includes("192.168")) return "test";
-		return "production";
+	previewOrigin() {
+		if (this.env() === "development") {
+			let arr = window.location.origin.split(":");
+			return `${arr[0]}:${arr[1]}:3000`
+		}
+		return window.location.origin
 	},
 	apiUrl() {
 		return "http://xxx.com/";
