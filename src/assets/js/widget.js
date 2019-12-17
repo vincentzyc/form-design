958e180f9474ace1e10033aca044e5d3b228853f
Vincentyun <vincentzyc@qq.com>
2019-12-17 17:30:41 +0800

    代码优化

--- src/assets/js/widget.js
+++ src/assets/js/widget.js
@@ -1,25 +1,25 @@
 export default {
-  "basicComponents": [
+  basicComponents: [
     {
-      "type": "phone",
-      "name": "手机号",
-      "placeholder": "请输入手机号",
-      "showLabel": false,
-      "label": {
-        "labelTitle": "手机号",
-        "labelPosition": "left",
-        "labelwidth": 50,
-        "labelWidth": "50px"
+      type: "phone",
+      name: "手机号",
+      placeholder: "请输入手机号",
+      showLabel: false,
+      label: {
+        labelTitle: "手机号",
+        labelPosition: "left",
+        labelwidth: 50,
+        labelWidth: "50px"
       },
-      "value": "",
-      "apiKey": "phone",
-      "codeValue": "",
-      "codeKey": "verifyCode",
-      "showCode": true,
-      "style": {
-        "margin": "0px 0px 0px 0px",
-        "btnStyle": {
-          "background": "#409EFF",
+      value: "",
+      apiKey: "phone",
+      codeValue: "",
+      codeKey: "verifyCode",
+      showCode: true,
+      style: {
+        margin: "0px 0px 0px 0px",
+        btnStyle: {
+          background: "#409EFF",
           color: "#fff",
           borderColor: "#409EFF",
           borderRadius: '5px',
