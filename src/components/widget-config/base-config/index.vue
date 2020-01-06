42e9d1d7d998a379cccee3b3cc8d8b86c90aa0d5
Vincentyun <vincentzyc@qq.com>
2020-01-06 10:36:16 +0800

    代码结构优化

---
+++ src/components/widget-config/base-config/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <Common :selectWg="selectWg" />
+    <WeChat v-if="selectWg.type==='wechat'" :selectWg="selectWg" />
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import Common from "./common"
+import WeChat from "./wechat"
+
+export default {
+  components: {
+    Common,
+    WeChat
+  },
+  props: {
+    selectWg: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState({
+      pageData: state => state.createh5.pageData,
+    })
+  }
+}
+</script>
