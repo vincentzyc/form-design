6a4847c8f155d5c79a4937b0429a5d070620b12b
Vincentyun <vincentzyc@qq.com>
2020-01-06 10:36:29 +0800

    新增微信关注组件

---
+++ src/components/widget-config/base-config/wechat.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-form-item label="微信名称">
+      <el-input v-model="selectWg.value" placeholder="请输入您的微信名称"></el-input>
+    </el-form-item>
+    <el-form-item label="微信账号">
+      <el-input v-model="selectWg.wechatName" placeholder="请输入您的微信账号"></el-input>
+    </el-form-item>
+    <el-form-item label="微信头像">
+      <ImgUpload :value.sync="selectWg.logo" />
+    </el-form-item>
+    <el-form-item label="显示微信头像">
+      <el-switch v-model="selectWg.showLogo"></el-switch>
+    </el-form-item>
+    <el-form-item label="显示微信名称">
+      <el-switch v-model="selectWg.showName"></el-switch>
+    </el-form-item>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  props: {
+    selectWg: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState({
+      predefineColors: state => state.createh5.predefineColors
+    })
+  }
+}
+</script>
