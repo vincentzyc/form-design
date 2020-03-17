fc53f807480b151b2e5cac5b5a4f3cf874dc08ef
Vincentyun <vincentzyc@qq.com>
2020-03-17 08:54:42 +0800

    图片组件新增多列

--- src/assets/js/widget.js
+++ src/assets/js/widget.js
@@ -252,14 +252,24 @@ export default {
       }
     },
     {
-      "type": "imgShow",
-      "name": "图片展示",
-      "value": "",
-      "link": "",
+      type: "imgShow",
+      name: "图片展示",
       fixedBottom:false,
       scrollHeight:0,
-      "style": {
-        "margin": "0px 0px 0px 0px"
+      styleType: "col1",
+      styleTypes: [{
+        value: "col1",
+        label: "单列"
+      }, {
+        value: "col2",
+        label: "双列"
+      }],
+      imglist: [{
+        img: "",
+        link: ""
+      }],
+      style: {
+        margin: "0px 0px 0px 0px"
       }
     }
   ],
