985e8e8e0ff7d5ea482a5857e3b20de4d3dcfd19
Vincentyun <vincentzyc@qq.com>
2020-03-17 08:55:09 +0800

    多列图片组件

--- src/components/widget-form-list/wg-imgshow.vue
+++ src/components/widget-form-list/wg-imgshow.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="wg-imgshow">
-    <div class="flex flex-center" :class="{'img-placeholder':!item.value}" :style="item.style">
-      <img v-if="item.value" :src="item.value" alt="图片展示" width="100%">
-      <img v-else src="@/assets/img/img-placeholder.png" alt="图片展示">
-    </div>
+  <div class="wg-imgshow" :style="item.style">
+    <ul class="flex flex-wrap">
+      <li :class="[item.styleType==='col1'?'col-12':'col-6']" v-for="(imgBtn,index) in item.imglist" :key="index">
+        <div class="flex flex-center" :class="{'img-placeholder':!imgBtn.img}">
+          <img v-if="imgBtn.img" :src="imgBtn.img" alt="图片按钮" width="100%" />
+          <img v-else src="@/assets/img/img-placeholder.png" alt="图片展示" />
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
