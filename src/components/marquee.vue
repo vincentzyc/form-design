6a0bec7b0a8b542372f4874fb3982029e9d497c4
vincentzyc <vincentzyc@qq.com>
2020-04-06 18:02:01 +0800

    marquee

--- src/components/marquee.vue
+++ src/components/marquee.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="my-outbox" ref="outbox">
-    <div class="my-inbox" ref="movebox">
-      <div class="my-listbox" v-for="(item,index) in realSendVal" :key="index">
-        <div class="my-title">
-          {{item.name}}
-          <text class="my-utel">{{item.mobile}}</text>
-          <text class="my-addr">{{item.place}}</text>
-        </div>
-        <div class="my-content">{{item.content}}</div>
-      </div>
+  <div class="marquee-outbox" ref="outbox">
+    <div
+      ref="movebox"
+      :class="`marquee-inbox-${this.direction}`"
+      :style="{animation:`marquee-${this.direction} ${this.durationTime}s linear infinite`}"
+    >
+      <slot></slot>
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'my-marquee-top',
+  name: 'my-marquee',
   props: {
     sendVal: Array,
     durationTime: {
@@ -27,25 +25,16 @@ export default {
       default: 'up'
     }
   },
-  data() {
-    return {
-      isShow: false
-    }
-  },
-  computed: {
-    realSendVal() {
-      if (this.sendVal.length > 0) {
-        return [...this.sendVal, ...this.sendVal]
-      } else {
-        return []
-      }
-    }
+  mounted() {
+    this.$nextTick(() => {
+      // this.$refs.movebox.style.animation = `marquee-${this.direction} ${this.durationTime}s linear infinite`
+    })
   }
 }
 </script>
 
-<style scoped>
-.my-outbox {
+<style>
+.marquee-outbox {
   color: #fee7b1;
   height: 300px;
   margin: 20px;
@@ -53,36 +42,19 @@ export default {
   overflow: hidden;
   border: 2px solid #c46302;
 }
-.my-outbox .my-inbox {
-  margin: 0 45px;
+.marquee-inbox-up {
   -webkit-animation: marquee-up 5s linear infinite;
+  -ms-animation: marquee-up 5s linear infinite;
+  -o-animation: marquee-up 5s linear infinite;
   animation: marquee-up 5s linear infinite;
 }
-.my-outbox .my-listbox {
-  padding: 20px 0;
-  font-size: 18px;
-  border-bottom: 1px solid #c7beb1;
+.marquee-inbox-left {
+  -webkit-animation: marquee-left 5s linear infinite;
+  -ms-animation: marquee-left 5s linear infinite;
+  -o-animation: marquee-left 5s linear infinite;
+  animation: marquee-left 5s linear infinite;
 }
-.my-outbox .my-title {
-  color: #db7000;
-  margin-bottom: 10px;
-  clear: both;
-  overflow: hidden;
-}
-.my-outbox .my-utel {
-  font-size: 16px;
-  margin-left: 20px;
-}
-.my-outbox .my-addr {
-  font-size: 16px;
-  float: right;
-}
-.my-outbox .my-content {
-  text-align: justify;
-  word-break: break-all;
-  font-size: 14px;
-  color: #53565d;
-}
+
 @keyframes marquee-up {
   0% {
     transform: translateY(0);
