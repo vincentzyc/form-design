8607de31c5d4517a5ec897fc23471206b53a9c5c
Vincentyun <vincentzyc@qq.com>
2020-04-15 13:46:47 +0800

    wg-agreement

---
+++ src/components/widget-form-list/wg-agreement.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wg-agreement clearfix">
+    <el-checkbox v-model="item.value" :style="item.style">
+      <p :style="{color:item.style.color}">
+        <span>{{item.tipText}}</span>
+        <span v-for="(titleText,key) in item.titleTexts" :key="key">
+          <span v-if="key>0">和</span>
+          <span :style="{color:item.agreementColor}" v-if="titleText.title">{{titleText.title}}</span>
+        </span>
+      </p>
+    </el-checkbox>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
