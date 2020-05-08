f9f9d14dd2377b036b0b9a22b548e11f47c2dc25
Vincentyun <vincentzyc@qq.com>
2020-05-08 10:15:10 +0800

    代码结构优化

--- src/assets/js/widget/index.js
+++ src/assets/js/widget/index.js
@@ -272,18 +272,7 @@ export default {
   {
     type: "marquee",
     name: "跑马灯",
-    fixedBottom: false,
-    scrollHeight: 0,
-    direction: "up", //滚动方向 向左=left  向上=up
-    durationTime: 6, //滚动时长 单位秒
-    isImgBtn: false,
-    imgUrl: "",
-    textList: [
-      '李小姐34秒前已报名',
-      '张先生1分钟前 已报名',
-      '周先生 3分钟前 已报名',
-      '梁小姐  5分钟前  已报名'
-    ],
+    marqueeType: "marquee1",
     style: {
       backgroundColor: "",
       Height: 50,
@@ -299,10 +288,10 @@ export default {
   }
   ],
   "assistComponents": [{
-    "type": "staticText",
-    "name": "文本描述",
-    "value": "文本描述",
-    "link": "",
+    type: "staticText",
+    name: "文本描述",
+    value: "文本描述",
+    link: "",
     backgroundImage: "",
     backgroundColor: "",
     style: {
@@ -332,10 +321,10 @@ export default {
     agreementColor: '#3b9cfd'
   },
   {
-    "type": "splitLine",
-    "name": "分割线",
-    "value": "solid",
-    "style": {
+    type: "splitLine",
+    name: "分割线",
+    value: "solid",
+    style: {
       "borderColor": "#d3d3d3",
       "borderStyle": "solid",
       "borderTopWidth": "1px",
---
+++ src/assets/js/widget/wg-marquee.js
@@ -0,0 +1,16 @@
+export default {
+  marquee1: {
+    fixedBottom: false,
+    scrollHeight: 0,
+    direction: "up", //滚动方向 向左=left  向上=up
+    durationTime: 6, //滚动时长 单位秒
+    isImgBtn: false,
+    imgUrl: "",
+    textList: [
+      '李小姐34秒前已报名',
+      '张先生1分钟前 已报名',
+      '周先生 3分钟前 已报名',
+      '梁小姐  5分钟前  已报名'
+    ]
+  }
+} 
