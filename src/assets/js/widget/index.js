67bd4ab918777835aa9d9a937b66d3be2096dcfe
Vincentyun <vincentzyc@qq.com>
2020-05-09 09:12:05 +0800

    组件模块支持多样式

--- src/assets/js/widget/index.js
+++ src/assets/js/widget/index.js
@@ -272,7 +272,19 @@ export default {
   {
     type: "marquee",
     name: "跑马灯",
-    marqueeType: "marquee1",
+    marqueeType: "marquee-base",
+    fixedBottom: false,
+    scrollHeight: 0,
+    direction: "up", //滚动方向 向左=left  向上=up
+    durationTime: 6, //滚动时长 单位秒
+    isImgBtn: false,
+    imgUrl: "",
+    textList: [
+      '李小姐34秒前已报名',
+      '张先生1分钟前 已报名',
+      '周先生 3分钟前 已报名',
+      '梁小姐  5分钟前  已报名'
+    ],
     style: {
       backgroundColor: "",
       Height: 50,
