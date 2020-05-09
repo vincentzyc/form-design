46cb04e73111a7fdb382812b73a5bdbe54fe80ef
Vincentyun <vincentzyc@qq.com>
2020-05-09 09:12:44 +0800

    跑马灯多样式

---
+++ src/components/widget-config/base-config/marquee/marquee-fixed.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-form-item label="时间（s）" v-if="selectWg.hasOwnProperty('durationTime')">
+      <el-input-number v-model="selectWg.durationTime" :min="1" :max="60" size="small" />
+    </el-form-item>
+  </div>
+</template>
+
+<script>
+// import Draggable from 'vuedraggable'
+export default {
+  components: {
+    // Draggable
+  },
+  props: {
+    selectWg: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+  }
+}
+</script>
