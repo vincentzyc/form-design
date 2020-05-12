97ef407a9ab2bf9389931918df68296371f7086a
Vincentyun <vincentzyc@qq.com>
2020-05-12 09:39:53 +0800

    代码结构优化

--- src/components/widget-config/base-config/marquee/index.vue
+++ src/components/widget-config/base-config/marquee/index.vue
@@ -1,26 +1,20 @@
 <template>
   <div>
-    <component :is="selectWg.marqueeType" :selectWg="selectWg" />
+    <component :is="selectWg.type" :selectWg="selectWg" />
   </div>
 </template>
 
 <script>
-import MarqueeList from '@/assets/js/widget/marquee-list'
-import MarqueeBase from './marquee-base'
+import Marquee from './marquee'
 
 export default {
   components: {
-    'marquee-base': MarqueeBase,
+    'marquee': Marquee,
   },
   props: {
     selectWg: {
       type: Object,
       required: true
-    }
-  },
-  data() {
-    return {
-      MarqueeList: MarqueeList
     }
   }
 }
