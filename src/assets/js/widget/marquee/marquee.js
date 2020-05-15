c5f1e86787ad47079ab08582ae3ab0b25e6d86bd
Vincentyun <vincentzyc@qq.com>
2020-05-16 01:05:13 +0800

    新增跑马灯组件

--- src/assets/js/widget/marquee/marquee.js
+++ src/assets/js/widget/marquee/marquee.js
@@ -25,4 +25,33 @@ export default [{
     lineHeight: "28px",
     margin: "0px 0px 0px 0px"
   }
+},{
+  type: "marqueeSingle",
+  name: "跑马灯-单个",
+  positionFixed: 'auto', // auto--正常 top--顶部悬浮  custom--自定义位置悬浮  bottom--底部悬浮
+  durationTime: 2, //滚动间隔 单位秒
+  textList: [{
+    text: '李**34秒前已报名',
+    sex: 'man'
+  }, {
+    text: '张**1分钟前已报名',
+    sex: 'woman'
+  }, {
+    text: '周**3分钟前已报名',
+    sex: 'man'
+  }, {
+    text: '梁**5分钟前已报名',
+    sex: 'woman'
+  }],
+  style: {
+    width: "200px",
+    pxWidth: 200,
+    backgroundColor: "#333",
+    Height: 40,
+    height: '40px',
+    color: "#fff",
+    fontsize: 14,
+    fontSize: "14px",
+    margin: "0px 0px 0px 0px"
+  }
 }]
