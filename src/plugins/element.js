082d58f423da98d7a8ac221e862b0f6cfa471976
Vincentyun <vincentzyc@qq.com>
2020-05-20 17:23:50 +0800

    add el-slider

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -40,7 +40,7 @@ import {
   // Tag,
   // Tree,
   // Alert,
-  // Slider,
+  Slider,
   // Icon,
   // Row,
   // Col,
@@ -110,7 +110,7 @@ Vue.use(FormItem);
 // Vue.use(Tag);
 // Vue.use(Tree);
 // Vue.use(Alert);
-// Vue.use(Slider);
+Vue.use(Slider);
 // Vue.use(Icon);
 // Vue.use(Row);
 // Vue.use(Col);
