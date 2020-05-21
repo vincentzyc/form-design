e7e986b06ffb15f4021da22bef38477851277b62
Vincentyun <vincentzyc@qq.com>
2020-05-21 09:08:11 +0800

    自定义悬浮跑马灯

--- src/components/widget-config/base-config/marquee-single.vue
+++ src/components/widget-config/base-config/marquee-single.vue
@@ -3,15 +3,24 @@
     <el-form-item label="滚动间隔（秒）" v-if="selectWg.hasOwnProperty('durationTime')">
       <el-input-number size="small" v-model="selectWg.durationTime" :min="1" :max="6" :step="0.5" step-strictly />
     </el-form-item>
-    <el-form-item label="内容高度(px)">
-      <el-input-number
-        v-model="selectWg.style.Height"
-        :min="10"
-        :max="100"
-        size="small"
-        @change="v=>selectWg.style.height=v+'px'"
-      />
-    </el-form-item>
+    <div v-if="selectWg.hasOwnProperty('position')">
+      <el-form-item label="悬浮位置">
+        <el-radio-group v-model="selectWg.position.side" size="small">
+          <el-radio-button label="left">左悬浮</el-radio-button>
+          <el-radio-button label="right">右悬浮</el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="左边距(%)" v-show="selectWg.position.side==='left'">
+        <el-slider v-model="selectWg.position.left" class="pd-l10 pd-r10"></el-slider>
+      </el-form-item>
+      <el-form-item label="右边距(%)" v-show="selectWg.position.side==='right'">
+        <el-slider v-model="selectWg.position.right" class="pd-l10 pd-r10"></el-slider>
+      </el-form-item>
+      <el-form-item label="上边距(px)">
+        <el-input-number v-model="selectWg.position.top" :precision="0" :min="0" :step="5" size="small" />
+      </el-form-item>
+    </div>
+
     <el-form-item label="选项" v-if="selectWg.hasOwnProperty('textList')">
       <Draggable tag="ul" :list="selectWg.textList" :group="{ name:'options'}" ghostClass="ghost" handle=".move-icon">
         <li v-for="(item, index) in selectWg.textList" :key="index">
