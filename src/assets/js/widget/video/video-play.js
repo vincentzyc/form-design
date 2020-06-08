9d0e2df955a17ac98c4b8abff6616a7be920f74d
Vincentyun <vincentzyc@qq.com>
2020-06-08 09:26:11 +0800

    新增视频播放

---
+++ src/assets/js/widget/video/video-play.js
@@ -0,0 +1,13 @@
+export default [{
+  type: "videoPlay",
+  name: "视频播放",
+  videoAttr: {
+    src: "",
+    poster: "",
+    autoplay: false,
+    height: ""
+  },
+  style: {
+    margin: "0px 0px 0px 0px"
+  }
+}]
