bef46dc33a3c463ff307e01191fcde4fee3f260e
Vincentyun <vincentzyc@qq.com>
2020-06-09 10:40:36 +0800

    新增视频播放组件

--- src/assets/js/widget/video/video-play.js
+++ src/assets/js/widget/video/video-play.js
@@ -2,10 +2,12 @@ export default [{
   type: "videoPlay",
   name: "视频播放",
   videoAttr: {
-    src: "",
-    poster: "",
+    src: "https://www.w3school.com.cn/i/movie.mp4",
+    poster: "https://www.w3school.com.cn/time/20200601/tiy.gif",
     autoplay: false,
-    height: ""
+    width: '100%',
+    height: 200,
+    controls:true
   },
   style: {
     margin: "0px 0px 0px 0px"
