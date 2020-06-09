4954eb370f24a4901ef37d2b8b1a9be8c59f22fe
Vincentyun <vincentzyc@qq.com>
2020-06-09 10:41:23 +0800

    新增视频播放组件

--- src/components/widget-config/base-config/video-play.vue
+++ src/components/widget-config/base-config/video-play.vue
@@ -3,7 +3,7 @@
     <el-form-item label="视频地址">
       <el-input size="small" v-model="selectWg.videoAttr.src" />
     </el-form-item>
-    <el-form-item label="封面图片">
+    <el-form-item label="封面图片地址">
       <el-input size="small" v-model="selectWg.videoAttr.poster" />
     </el-form-item>
     <el-form-item label="是否自动播放">
@@ -11,9 +11,9 @@
     </el-form-item>
     <el-form-item label="内容高度(px)">
       <el-input-number
-        v-model="selectWg.videoAttr.Height"
-        :min="50"
-        :max="200"
+        v-model="selectWg.videoAttr.height"
+        :min="100"
+        :max="300"
         size="small"
       />
     </el-form-item>
