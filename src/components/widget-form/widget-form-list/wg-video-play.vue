69bc1669d747b93427ecf51aa115da7a1a7ff2d9
Vincentyun <vincentzyc@qq.com>
2020-06-09 10:41:09 +0800

    新增视频播放组件

---
+++ src/components/widget-form/widget-form-list/wg-video-play.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wg-video-play" :style="item.style">
+    <video v-bind="item.videoAttr">您的浏览器不支持视频播放哦~</video>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
