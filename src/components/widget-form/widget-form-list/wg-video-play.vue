b2f9a2da6c7c506372c97d514a3076d427fdbdf0
Vincentyun <vincentzyc@qq.com>
2020-06-10 08:56:13 +0800

    add video play

--- src/components/widget-form/widget-form-list/wg-video-play.vue
+++ src/components/widget-form/widget-form-list/wg-video-play.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="wg-video-play" :style="item.style">
-    <video v-bind="item.videoAttr">您的浏览器不支持视频播放哦~</video>
+  <div class="wg-video-play transition03" :style="item.style">
+    <video v-bind="item.videoAttr" style="width:100%;height:100%">您的浏览器不支持视频播放哦~</video>
   </div>
 </template>
 
