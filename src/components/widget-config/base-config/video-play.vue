91bf9cf0184136b5c9e53b66afcff08d72864ba7
Vincentyun <vincentzyc@qq.com>
2020-06-10 08:55:33 +0800

    新增视频组件配置项

--- src/components/widget-config/base-config/video-play.vue
+++ src/components/widget-config/base-config/video-play.vue
@@ -9,14 +9,6 @@
     <el-form-item label="是否自动播放">
       <el-switch v-model="selectWg.videoAttr.autoplay"></el-switch>
     </el-form-item>
-    <el-form-item label="内容高度(px)">
-      <el-input-number
-        v-model="selectWg.videoAttr.height"
-        :min="100"
-        :max="300"
-        size="small"
-      />
-    </el-form-item>
   </div>
 </template>
 
