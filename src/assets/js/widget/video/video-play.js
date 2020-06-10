8135ef43cce1a09ab1ec374f676b697501947e0a
Vincentyun <vincentzyc@qq.com>
2020-06-10 08:55:00 +0800

    样式优化

--- src/assets/js/widget/video/video-play.js
+++ src/assets/js/widget/video/video-play.js
@@ -2,14 +2,15 @@ export default [{
   type: "videoPlay",
   name: "视频播放",
   videoAttr: {
-    src: "https://www.w3school.com.cn/i/movie.mp4",
-    poster: "https://www.w3school.com.cn/time/20200601/tiy.gif",
+    src: "https://www.runoob.com/try/demo_source/movie.mp4",
+    poster: "https://www.runoob.com/images/logo.png",
     autoplay: false,
-    width: '100%',
-    height: 200,
-    controls:true
+    controls: true
   },
   style: {
+    width: '100%',
+    pxHeight: 200,
+    height: '200px',
     margin: "0px 0px 0px 0px"
   }
 }]
