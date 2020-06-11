0dfe173fb63063f73034fb8949d79c15ffe240b3
Vincentyun <vincentzyc@qq.com>
2020-06-11 18:18:04 +0800

    add el-progress

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -45,7 +45,7 @@ import {
   // Row,
   // Col,
   Upload,
-  // Progress,
+  Progress,
   // Badge,
   // Card,
   // Rate,
@@ -115,7 +115,7 @@ Vue.use(Slider);
 // Vue.use(Row);
 // Vue.use(Col);
 Vue.use(Upload);
-// Vue.use(Progress);
+Vue.use(Progress);
 // Vue.use(Badge);
 // Vue.use(Card);
 // Vue.use(Rate);
