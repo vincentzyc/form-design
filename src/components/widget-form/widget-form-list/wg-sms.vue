c266295e34abf0f2c494d46f7fe8db3c1041d37b
Vincentyun <vincentzyc@qq.com>
2020-06-16 09:09:29 +0800

    add wg-sms

---
+++ src/components/widget-form/widget-form-list/wg-sms.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :style="item.style">
+    <ul class="flex flex-wrap">
+      <li :class="[item.styleType==='col1'?'col-12':'col-6']" v-for="(smsBtn,index) in item.smslist" :key="index">
+        <div class="flex flex-center" :style="{padding:item.style.btnPadding}">
+          <div v-if="item.isImgBtn" :class="{'img-placeholder':!smsBtn.img}">
+            <img v-if="smsBtn.img" :src="smsBtn.img" alt="图片按钮" width="100%" />
+            <img v-else src="@/assets/img/img-placeholder.png" alt="图片展示" />
+          </div>
+          <button class="wg-button" :style="item.style.btnStyle" v-else>{{smsBtn.btnText}}</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
