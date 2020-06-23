08eeab28a0084e897996dfd0ece97f2e71e4a20b
Vincentyun <vincentzyc@qq.com>
2020-06-23 09:16:50 +0800

    新增返回劫持功能

--- src/components/page-config/index.vue
+++ src/components/page-config/index.vue
@@ -10,6 +10,23 @@
       <el-form-item label="页面背景图片">
         <FileUpload :value.sync="pageData.style.backgroundImage" />
       </el-form-item>
+      <el-form-item label="是否劫持返回">
+        <el-switch v-model="pageData.hijackBack.isHijack"></el-switch>
+      </el-form-item>
+      <template v-if="pageData.hijackBack.isHijack">
+        <el-form-item label="劫持返回弹窗图片">
+          <FileUpload :value.sync="pageData.hijackBack.alertImg" />
+        </el-form-item>
+        <el-form-item label="点击图片跳转链接">
+          <el-input
+            type="textarea"
+            placeholder="请输入跳转链接"
+            :autosize="{ minRows: 2, maxRows: 6}"
+            v-model.trim="pageData.hijackBack.alertLink"
+            @blur="()=>$util.checkLink(pageData.hijackBack.alertLink)"
+          ></el-input>
+        </el-form-item>
+      </template>
       <el-form-item label="主题" v-if="themes.length>0">
         <el-radio-group v-model="pageData.theme" @change="setTheme">
           <el-radio-button v-for="item in themes" :label="item.value" :key="item.value">{{item.name}}</el-radio-button>
