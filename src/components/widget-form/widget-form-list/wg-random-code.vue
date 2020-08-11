af71747f820ae6efc65208391f3a8fdf1d0f1cf0
Vincentyun <vincentzyc@qq.com>
2020-08-11 10:00:36 +0800

    新增随机码组件

---
+++ src/components/widget-form/widget-form-list/wg-random-code.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wg-random-code wg-padding" :style="item.style">
+    <div class="wg-item" :class="[item.label.labelPosition==='top'?'flex-column':'align-middle']">
+      <div class="wg-title" :style="{width:item.label.labelWidth}" v-show="item.showLabel">{{item.label.labelTitle}}</div>
+      <div class="flex flex-auto align-middle">
+        <input class="wg-input" v-model="item.value" :placeholder="item.placeholder" />
+        <button class="random-code flex-none" :style="item.style.btnStyle">AaZ1</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
