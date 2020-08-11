e3f2740932e2c9fff760aaf03cebbed7b995530c
Vincentyun <vincentzyc@qq.com>
2020-08-11 10:00:01 +0800

    新增随机码组件

---
+++ src/assets/js/widget/form/random-code.js
@@ -0,0 +1,27 @@
+export default [{
+  type: "randomCode",
+  name: "随机码",
+  placeholder: "请输入验证码",
+  showLabel: false,
+  label: {
+    labelTitle: "验证码",
+    labelPosition: "left",
+    labelwidth: 66,
+    labelWidth: "66px"
+  },
+  value: "",
+  apiKey: "randomCode",
+  codeValue: "",
+  style: {
+    margin: "0px 0px 0px 0px",
+    btnStyle: {
+      fontFamily: 'Helvetica Neue',
+      backgroundColor: "#409EFF",
+      color: "#fff",
+      fontSize: "20px",
+      fontsize: 20,
+      borderRadius: '5px',
+      borderradius: 5,
+    }
+  }
+}]
