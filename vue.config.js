3bd0aa9d3046416fe359a46f7569e8240e524796
Vincentyun <vincentzyc@qq.com>
2020-11-03 10:43:11 +0800

    cdn

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,19 @@ module.exports = {
   publicPath: "./",
   outputDir: "docs",
   productionSourceMap: false,
+  configureWebpack: {
+    // 以下库使用cdn，不会被打包
+    externals: process.env.NODE_ENV === 'production' ? {
+      "vue": "Vue",
+      "vue-router": 'VueRouter',
+      "vuex": "Vuex",
+      "quill": "Quill",
+      "vue-quill-editor": "VueQuillEditor",
+      "vuedraggable": "vuedraggable"
+    } : {}
+  },
   chainWebpack: config => {
+    config.plugins.delete('preload')
     config.plugins.delete('prefetch')
     config.module
       .rule('images')
