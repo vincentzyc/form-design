7e146b18abc1ae0dc37d0627e9f880815aa0bf96
Vincentyun <vincentzyc@qq.com>
2020-11-15 11:12:18 +0800

    ✨feat: 新增按钮动画功能

---
+++ src/components/widget-config/animation.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <el-form-item label="动画选择" v-if="selectWg.style.hasOwnProperty('animationName')">
+      <ul>
+        <li>呼吸灯</li>
+        <li>渐变</li>
+      </ul>
+    </el-form-item>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    selectWg: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
