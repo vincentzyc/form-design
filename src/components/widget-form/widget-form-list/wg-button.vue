fc8f286e9ff77ddf847a1c35e2885052b3ba79b9
Vincentyun <vincentzyc@qq.com>
2020-11-17 17:52:38 +0800

    🎈perf: 动画设计优化

--- src/components/widget-form/widget-form-list/wg-button.vue
+++ src/components/widget-form/widget-form-list/wg-button.vue
@@ -1,5 +1,6 @@
 <template>
   <div :class="wrapClass" :style="wrapStyle" class="flex flex-center">
+    <span :style="animteStyle" class="animte-el"></span>
     <div :class="{'img-placeholder':!item.style.value}" v-if="item.style.isImgBtn">
       <img :src="item.style.value" alt="图片按钮" v-if="item.style.value" width="100%" />
       <img alt="图片展示" src="@/assets/img/img-placeholder.png" v-else />
@@ -22,6 +23,13 @@ export default {
       return {
         ...this.item.style,
         ...this.item.animation
+      }
+    },
+    animteStyle() {
+      if (!this.item.animation?.className) return { display: 'none' }
+      return {
+        ...this.item.animation,
+        animationName: this.item.animation.className
       }
     },
     wrapClass() {
