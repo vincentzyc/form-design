cf48ba95aacb756175a189dd4640abcf65aa0d27
vincentzyc <vincentzyc@qq.com>
2020-12-03 17:09:42 +0800

    config🔧 no-console: 'off'

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
