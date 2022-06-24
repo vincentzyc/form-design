b1810c3aef4633c6364d495d5ca552fa22a2dfe1
vincentzyc <vincentzyc@qq.com>
2022-06-24 08:47:12 +0800

    ✨ feat: 新增倒计时组件

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -26,7 +26,7 @@ import {
   ButtonGroup,
   // Table,
   // TableColumn,
-  // DatePicker,
+  DatePicker,
   // TimeSelect,
   // TimePicker,
   // Popover,
@@ -97,7 +97,7 @@ Vue.use(Button);
 Vue.use(ButtonGroup);
 // Vue.use(Table);
 // Vue.use(TableColumn);
-// Vue.use(DatePicker);
+Vue.use(DatePicker);
 // Vue.use(TimeSelect);
 // Vue.use(TimePicker);
 // Vue.use(Popover);
