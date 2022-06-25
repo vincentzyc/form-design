2ddfb0b718002b1a8fd8850419fff9eb395a657c
vincentzyc <vincentzyc@qq.com>
2022-06-26 00:04:05 +0800

    ✨ feat: 新增倒计时组件

---
+++ src/components/widget-config/style-config/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <component :is="wgNameMap[selectWg.type]" :selectWg="selectWg" v-if="showWgName" />
+    <Common :selectWg="selectWg" />
+  </section>
+</template>
+
+<script>
+import Common from "./common"
+export default {
+  props: {
+    selectWg: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    Common,
+  },
+  data() {
+    return {
+      wgNameMap: {
+        countDown: () => import("./count-down")
+      }
+    }
+  },
+  computed: {
+    showWgName() {
+      if (!this.wgNameMap[this.selectWg.type]) return false
+      return true
+    }
+  }
+}
+</script>
