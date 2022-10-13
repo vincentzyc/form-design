43c7131447a043c768beaa82a1622a331d841af3
vincentzyc <vincentzyc@qq.com>
2022-10-13 21:06:04 +0800

    ✨ feat: 新增滑块校验组件

---
+++ src/components/widget-form/widget-form-list/wg-drag-verify.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    这是滑块校验
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
