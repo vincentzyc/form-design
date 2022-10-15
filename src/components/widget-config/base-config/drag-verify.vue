a08d03a5db081bf66e4696653860d55c66c3f907
vincentzyc <vincentzyc@qq.com>
2022-10-15 17:47:58 +0800

    ✨ feat: 新增滑块校验组件

---
+++ src/components/widget-config/base-config/drag-verify.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-form-item label="验证通过提示语">
+      <el-input size="small" v-model="selectWg.successText" />
+    </el-form-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectWg: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
