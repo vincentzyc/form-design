00a5d0df2fd8fb0464ddcaa61a4e68d58b45c8cf
vincentzyc <vincentzyc@qq.com>
2022-10-16 15:10:59 +0800

    ✨ feat: 新增滑块校验组件

--- src/components/base/DragVerify.vue
+++ src/components/base/DragVerify.vue
@@ -9,42 +9,40 @@
     @touchmove="dragMoving"
     @touchend="dragFinish"
   >
-
-    <div
-      class="dv_progress_bar"
-      :class="{goFirst2:isOk}"
-      ref="progressBar"
-      :style="progressBarStyle"
-    >
-
-    </div>
-    <div
-      class="dv_text"
-      :style="textStyle"
-      ref="message"
-    >
-      <slot
-        name="textBefore"
-        v-if="$slots.textBefore"
-      ></slot>
-      {{message}}
-      <slot
-        name="textAfter"
-        v-if="$slots.textAfter"
-      ></slot>
+    <div class="dv_progress_bar" :class="{ goFirst2: isOk }" ref="progressBar" :style="progressBarStyle"> </div>
+    <div class="dv_text" :style="textStyle" ref="message">
+      <slot name="textBefore" v-if="$slots.textBefore"></slot>
+      {{ message }}
+      <slot name="textAfter" v-if="$slots.textAfter"></slot>
     </div>
 
     <div
       class="dv_handler dv_handler_bg"
-      :class="{goFirst:isOk}"
+      :class="{ goFirst: isOk }"
       @mousedown="dragStart"
       @touchstart="dragStart"
       ref="handler"
       :style="handlerStyle"
     >
-      <i :class="handlerIcon"></i>
+      <div style="width: 24px; height: 24px">
+        <svg v-if="isPassing" viewBox="0 0 1024 1024">
+          <path
+            fill="#76c61d"
+            d="M512 960C264 960 64 759 64 512S264 64 512 64s448 200 448 448S759 960 512 960zM512 128C300 128 128 300 128 512c0 211 172 383 383 383 211 0 383-172 383-383C895 300 723 128 512 128z"
+          ></path>
+          <path
+            fill="#76c61d"
+            d="M726 393c-12-12-32-12-45 0l-233 235-103-106c-12-12-32-12-45-0-12 12-12 32-0 45l126 129c0 0 0 0 0 0 0 0 0 0 0 0 2 1 4 3 6 4 1 0 2 1 3 2 3 1 8 2 12 2 4 0 8-0 11-2 1-0 2-1 3-2 2-1 4-2 6-4 0-0 0-0 0-0 0-0 0-0 0-0l256-259C739 425 739 405 726 393z"
+          ></path>
+        </svg>
+        <svg v-else viewBox="0 0 1027 1024">
+          <path
+            fill="#8a8a8a"
+            d="M189 794c-10 0-20-3-28-11-15-15-15-40 0-56L408 479 160 231c-15-15-15-40 0-56 15-15 40-15 56 0l275 275c7 7 11 17 11 28 0 10-4 20-11 28L217 782C209 790 199 794 189 794zM557 794c-10 0-20-3-28-11-15-15-15-40 0-56l246-246L528 231c-15-15-15-40 0-56 15-15 40-15 56 0l275 275c15 15 15 40 0 56L585 782C578 790 567 794 557 794z"
+          ></path>
+        </svg>
+      </div>
     </div>
-
   </div>
 </template>
 <script>
@@ -53,76 +51,75 @@ export default {
   props: {
     isPassing: {
       type: Boolean,
-      default: false
+      default: false,
     },
     width: {
       type: Number,
-      default: 250
+      default: 250,
     },
     height: {
       type: Number,
-      default: 40
+      default: 40,
     },
     text: {
       type: String,
-      default: "swiping to the right side"
+      default: "swiping to the right side",
     },
     successText: {
       type: String,
-      default: "success"
+      default: "success",
     },
     background: {
       type: String,
-      default: "#eee"
+      default: "#eee",
     },
     progressBarBg: {
       type: String,
-      default: "#76c61d"
+      default: "#76c61d",
     },
     completedBg: {
       type: String,
-      default: "#76c61d"
+      default: "#76c61d",
     },
     circle: {
       type: Boolean,
-      default: false
+      default: false,
     },
     radius: {
       type: String,
-      default: "4px"
+      default: "4px",
     },
     handlerIcon: {
-      type: String
+      type: String,
     },
     successIcon: {
-      type: String
+      type: String,
     },
     handlerBg: {
       type: String,
-      default: "#fff"
+      default: "#fff",
     },
     textSize: {
       type: String,
-      default: "14px"
+      default: "14px",
     },
     textColor: {
       type: String,
-      default: "#333"
-    }
+      default: "#333",
+    },
   },
   mounted: function() {
     const dragEl = this.$refs.dragVerify;
     dragEl.style.setProperty("--textColor", this.textColor);
     dragEl.style.setProperty("--width", Math.floor(this.width / 2) + "px");
     dragEl.style.setProperty("--pwidth", -Math.floor(this.width / 2) + "px");
-    console.log(this.$slots);
   },
   computed: {
     handlerStyle: function() {
       return {
         width: this.height + "px",
         height: this.height + "px",
-        background: this.handlerBg
+        background: this.handlerBg,
       };
     },
     message: function() {
@@ -134,42 +131,41 @@ export default {
         height: this.height + "px",
         lineHeight: this.height + "px",
         background: this.background,
-        borderRadius: this.circle ? this.height / 2 + "px" : this.radius
+        borderRadius: this.circle ? this.height / 2 + "px" : this.radius,
       };
     },
     progressBarStyle: function() {
       return {
         background: this.progressBarBg,
         height: this.height + "px",
-        borderRadius: this.circle
-          ? this.height / 2 + "px 0 0 " + this.height / 2 + "px"
-          : this.radius
+        borderRadius: this.circle ? this.height / 2 + "px 0 0 " + this.height / 2 + "px" : this.radius,
       };
     },
     textStyle: function() {
       return {
         height: this.height + "px",
         width: this.width + "px",
-        fontSize: this.textSize
+        fontSize: this.textSize,
       };
-    }
+    },
   },
   data() {
     return {
       isMoving: false,
       x: 0,
-      isOk: false
+      isOk: false,
     };
   },
   methods: {
-    dragStart: function(e) {
+    dragStart(e) {
+      e.preventDefault();
       if (!this.isPassing) {
         this.isMoving = true;
-        this.x = (e.pageX || e.touches[0].pageX)
+        this.x = e.pageX || e.touches[0].pageX;
       }
       this.$emit("handlerMove");
     },
-    dragMoving: function(e) {
+    dragMoving(e) {
       if (this.isMoving && !this.isPassing) {
         var _x = (e.pageX || e.touches[0].pageX) - this.x;
         var handler = this.$refs.handler;
@@ -178,62 +174,58 @@ export default {
           this.$refs.progressBar.style.width = _x + this.height / 2 + "px";
         } else if (_x > this.width - this.height) {
           handler.style.left = this.width - this.height + "px";
-          this.$refs.progressBar.style.width =
-            this.width - this.height / 2 + "px";
+          this.$refs.progressBar.style.width = this.width - this.height / 2 + "px";
           this.passVerify();
         }
       }
     },
-    dragFinish: function(e) {
+    dragFinish(e) {
       if (this.isMoving && !this.isPassing) {
         var _x = (e.pageX || e.changedTouches[0].pageX) - this.x;
         if (_x < this.width - this.height) {
           this.isOk = true;
-          var that = this;
-          setTimeout(function() {
-            that.$refs.handler.style.left = "0";
-            that.$refs.progressBar.style.width = "0";
-            that.isOk = false;
+          setTimeout(() => {
+            this.$refs.handler.style.left = "0";
+            this.$refs.progressBar.style.width = "0";
+            this.isOk = false;
           }, 500);
           this.$emit("passfail");
         } else {
           var handler = this.$refs.handler;
           handler.style.left = this.width - this.height + "px";
-          this.$refs.progressBar.style.width =
-            this.width - this.height / 2 + "px";
+          this.$refs.progressBar.style.width = this.width - this.height / 2 + "px";
           this.passVerify();
         }
         this.isMoving = false;
       }
     },
-    passVerify: function() {
+    passVerify() {
       this.$emit("update:isPassing", true);
       this.isMoving = false;
-      var handler = this.$refs.handler;
+      const handler = this.$refs.handler;
       handler.children[0].className = this.successIcon;
       this.$refs.progressBar.style.background = this.completedBg;
       this.$refs.message.style["-webkit-text-fill-color"] = "unset";
-      this.$refs.message.style.animation = "slidetounlock2 3s infinite";
       this.$refs.message.style.color = "#fff";
       this.$emit("passcallback");
     },
-    reset: function() {
+    reset() {
       const oriData = this.$options.data();
       for (const key in oriData) {
         if (Object.prototype.hasOwnProperty.call(oriData, key)) {
-          this[key] = oriData[key]
+          this[key] = oriData[key];
         }
       }
-      var handler = this.$refs.handler;
-      var message = this.$refs.message;
+      this.$emit("update:isPassing", false);
+      const handler = this.$refs.handler;
+      const message = this.$refs.message;
       handler.style.left = "0";
       this.$refs.progressBar.style.width = "0";
       handler.children[0].className = this.handlerIcon;
       message.style["-webkit-text-fill-color"] = "transparent";
-      message.style.animation = "slidetounlock 3s infinite";
       message.style.color = this.background;
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
@@ -242,21 +234,18 @@ export default {
   background-color: #e8e8e8;
   text-align: center;
   overflow: hidden;
+  margin: auto;
+  border: 1px solid #eee;
 }
 .drag_verify .dv_handler {
   position: absolute;
   top: 0px;
   left: 0px;
   cursor: move;
-}
-.drag_verify .dv_handler i {
-  color: #666;
-  padding-left: 0;
-  font-size: 16px;
-}
-.drag_verify .dv_handler .el-icon-circle-check {
-  color: #6c6;
-  margin-top: 9px;
+  cursor: move;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .drag_verify .dv_progress_bar {
   position: absolute;
@@ -306,14 +295,6 @@ export default {
   }
   100% {
     background-position: var(--width) 0;
-  }
-}
-@keyframes slidetounlock2 {
-  0% {
-    background-position: var(--pwidth) 0;
-  }
-  100% {
-    background-position: var(--pwidth) 0;
   }
 }
 </style>
