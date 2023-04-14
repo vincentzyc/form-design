3c30d6547d48c7af8bce48cec908d6874a14af2b
vincentzyc <vincentzyc@qq.com>
2023-04-14 10:37:47 +0800

    🐳chore: vue2.7

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,26 @@
-module.exports = {
-  publicPath: "./",
-  outputDir: "docs",
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './',
+  outputDir: 'docs',
   productionSourceMap: false,
   configureWebpack: {
     // 以下库使用cdn，不会被打包
-    externals: process.env.NODE_ENV === 'production' ? {
-      "vue": "Vue",
-      "vue-router": 'VueRouter',
-      "vuex": "Vuex",
-      "quill": "Quill",
-      "vue-quill-editor": "VueQuillEditor",
-      "vuedraggable": "vuedraggable",
-      "compressorjs": "Compressor"
-    } : {}
-  },
-  chainWebpack: config => {
-    config.plugins.delete('preload')
-    config.plugins.delete('prefetch')
-    config.module
-      .rule('images')
-      .use('url-loader')
-      .loader('url-loader')
-      .tap(options => Object.assign(options, { limit: 10240 }))
+    externals:
+      process.env.NODE_ENV === 'production'
+        ? {
+            vue: 'Vue',
+            'vue-router': 'VueRouter',
+            vuex: 'Vuex',
+            quill: 'Quill',
+            'vue-quill-editor': 'VueQuillEditor',
+            vuedraggable: 'vuedraggable',
+            compressorjs: 'Compressor',
+            'element-ui': 'ELEMENT'
+          }
+        : {},
   },
   devServer: {
-    port: 9000
-  }
-};
+    port: 9000,
+  },
+});
