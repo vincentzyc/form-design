53711e4bb1b3e40a0b61f70c6b4ff3e9ef66fbcf
vincentzyc <vincentzyc@qq.com>
2023-04-14 10:37:54 +0800

    🐳chore: vue2.7

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  "presets": [
-    "@vue/app"
-  ],
-  "plugins": [
-    [
-      "component",
-      {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
-    ],
-    ["@babel/plugin-proposal-optional-chaining"]  //可选链语法
-  ],
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
