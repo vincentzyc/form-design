20d9a403ddf688c2b587e7259d5f90709a8bcb76
vincentzyc <vincentzyc@qq.com>
2023-04-17 10:36:34 +0800

    🎨refactor: 代码优化

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="flex flex-center" style="height:100%">
-    <img :src="BASE_URL+'static/img/404.jpg'" alt>
+    <img :src="BASE_URL+'static/img/404.jpg'" alt="404">
   </div>
 </template>
-
-<script>
-export default {};
-</script>
