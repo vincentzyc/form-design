c06069bc6194895bb7e75882832dcca1e25f6c7a
vincentzyc <vincentzyc@qq.com>
2023-04-17 10:37:08 +0800

    🎨refactor: script setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-mutating-props': 'off',
-    'vue/multi-word-component-names': 'off'
-  }
-}
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+  },
+};
