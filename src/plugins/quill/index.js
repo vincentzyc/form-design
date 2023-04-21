14dca97083afdea3ff5adfe32be29f2e12fa6905
vincentzyc <vincentzyc@qq.com>
2023-04-21 09:21:01 +0800

    🚀publish: github pages

--- src/plugins/quill/index.js
+++ src/plugins/quill/index.js
@@ -11,7 +11,6 @@ let AlignStyle = Quill.import('attributors/style/align');
 
 SizeStyle.whitelist = ['8px', '10px', '12px', '14px', '16px', '18px', '20px', '24px', '36px', '48px', '64px'];
 
-
 Quill.register(SizeStyle, true);
 Quill.register(AlignStyle, true);
 Quill.register("formats/textIndent", TextIndent, true);
